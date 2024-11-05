141f13c041f2a6ec37e3f5c90a10ffd47c723b36
OddyChance <odychance96@gmail.com>
2024-11-04 21:17:07 -0300

    First Commit

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,55 @@
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { SliceComponentProps, PrismicRichText, PrismicText } from "@prismicio/react";
+import ButtonLink from "@/components/ButtonLink";
+import Bounded from "@/components/Bounded";
+import StarGrid from "@/components/StarGrid";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="text-center"
+    >
+      <div className="relatice">
+        <StarGrid />
+        {isFilled.richText(slice.primary.heading) && (
+          <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
+            <PrismicText field={slice.primary.heading} />
+          </h1>
+        )}
+
+        {isFilled.richText(slice.primary.body) && (
+          <div className="mx-auto mt-6 max-w-md text-balance text-slate-300">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+        )}
+
+        {isFilled.link(slice.primary.button_link) && (
+          <ButtonLink className="mt-8" field={slice.primary.button_link}>
+            {slice.primary.button_label}
+          </ButtonLink>
+        )}
+
+        {isFilled.image(slice.primary.image) && (
+          <div className="glass-container mt-16 w-fit">
+            <div className="absolute inset-0 -z-10 blur-2xl filter bg-yellow-500/40"/>
+            <PrismicNextImage className="rounded-lg" field={slice.primary.image} />
+          </div>
+        )}
+
+      </div>
+    </Bounded>
+  );
+};
+
+export default Hero;
